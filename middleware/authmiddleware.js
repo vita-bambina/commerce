4ff6d05cc487e673178af82e11d2c784a3390422
Blessing <bambinablessing2214@gmail.com>
2026-05-11 13:36:08 +0100

    update

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -27,4 +27,14 @@ const authMiddleware = (req, res, next) => {
     }
 };
 
-module.exports = authMiddleware;
+const adminMiddleware = (req, res, next) => {
+    if (req.user.role === "admin") {
+        next(); // allow access
+    } else {
+        return res.status(403).json({
+            message: "Admins only"
+        });
+    }
+};
+
+module.exports = {adminMiddleware, authMiddleware};
--- routes/productroute.js
+++ routes/productroute.js
@@ -2,14 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware =
-require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
 const {createProduct,getProducts,getSingleProduct} = require("../controllers/productController");
 
 
 // protected
 router.post( "/products",authMiddleware,createProduct);
+router.post("/add", authMiddleware, adminMiddleware, addProduct);
 
 
 // public
