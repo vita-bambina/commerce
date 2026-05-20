3edecb7ed6ff7a9adda0722c1a0bf895c28e569f
Blessing <bambinablessing2214@gmail.com>
2026-05-20 19:10:51 +0100

    initial backend + prisma setup

--- routes/productroute.js
+++ routes/productroute.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authmiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 const upload = require("../middleware/upload");
 
