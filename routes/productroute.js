0b541e523ae9e821bcef7584f6f66ac0af60a08c
Blessing <bambinablessing2214@gmail.com>
2026-05-20 19:16:46 +0100

    fix middleware path for production

--- routes/productroute.js
+++ routes/productroute.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authmiddleware");
-const adminMiddleware = require("../middleware/adminMiddleware");
+const authmiddleware = require("../middleware/authmiddleware");
+const adminmiddleware = require("../middleware/adminmiddleware");
 const upload = require("../middleware/upload");
 
 const {addProducts,getProducts,getSingleProduct,} = require("../controllers/productController");
 // protected
-router.post( "/addProducts",authMiddleware, adminMiddleware,upload.single("image"), addProducts);
+router.post( "/addProducts",authmiddleware, adminmiddleware,upload.single("image"), addProducts);
 
 
 // public
